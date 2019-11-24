494df680a32a61b22a97b0567c0959a1c81406da
andres-elias-vargas-cenfotec <avargasr@ucenfotec.ac.cr>
2019-11-23 19:44:39 -0600

    Actualización
    - Actualización de contenidos y org de eventos.

--- js/registrar_organizador_eventos_usuario/registrar-organizador-eventos-imagen.js
+++ js/registrar_organizador_eventos_usuario/registrar-organizador-eventos-imagen.js
@@ -2,7 +2,7 @@
 
 var myWidget1 = cloudinary.createUploadWidget({
     cloudName: 'belegurth',
-    uploadPreset: 'logo_orgEventos'
+    uploadPreset: 'fotosProyectoFinal'
 }, (error, result) => {
     if (!error && result && result.event === "success") {
         console.log('Done! Here is the image info: ', result.info);
