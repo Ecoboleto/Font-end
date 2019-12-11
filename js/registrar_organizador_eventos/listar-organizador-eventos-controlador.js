277ed19139248202f3801249e96f9385cbbab701
andres-elias-vargas-cenfotec <avargasr@ucenfotec.ac.cr>
2019-12-10 23:54:18 -0600

    Listar Organizador de eventos
    - Listar Organizador de eventos

--- js/registrar_organizador_eventos/listar-organizador-eventos-controlador.js
+++ js/registrar_organizador_eventos/listar-organizador-eventos-controlador.js
@@ -5,33 +5,33 @@ const input_filtro = document.querySelector('#txt_filtro_organizador_eventos')
 let lista_organizadores;
 
 let llenarTabla = async () => {
-    let filtro = input_filtro.value.toLowerCase();
-    lista_organizadores = await listar_organizador_evento();
-    console.log(lista_organizadores);
+    // let filtro = input_filtro.value.toLowerCase();
+    lista_organizadores = await listar_organizador_evento(); 
     
     tbody.innerHTML = '';
     for (let i = 0; i < lista_organizadores.length; i++) {
-        let nombre = lista_organizadores[i]['nombre_completo'].toLowerCase();
-        if (nombre.includes(filtro)) {
+        // let nombre = lista_organizadores[i]['nombre_completo'].toLowerCase();
+        
+        // if (nombre.includes(filtro)) {
+        if( lista_organizadores[i].tipo_usuario == 'organizador_evento' ){
             let fila = tbody.insertRow();
 
             fila.insertCell().innerText = lista_organizadores[i]['nombre_empresa'];
-            fila.insertCell().innerText = lista_organizadores[i]['nombre_comercial'];
-            fila.insertCell().innerText = lista_organizadores[i]['log'];
+            // fila.insertCell().innerText = lista_organizadores[i]['nombre_comercial'];
+            fila.insertCell().innerHTML = '<img src="'+ lista_organizadores[i]['log'] + '" class="logo-empresas">';
             fila.insertCell().innerText = lista_organizadores[i]['tipo_cedula'];
             fila.insertCell().innerText = lista_organizadores[i]['cedula'];
             fila.insertCell().innerText = lista_organizadores[i]['anos_experiencia'];
             fila.insertCell().innerText = lista_organizadores[i]['provincia'];
             fila.insertCell().innerText = lista_organizadores[i]['canton'];
             fila.insertCell().innerText = lista_organizadores[i]['distrito'];
-            fila.insertCell().innerText = lista_organizadores[i]['direccion_exacta'];
+            // fila.insertCell().innerText = lista_organizadores[i]['direccion_exacta'];
             fila.insertCell().innerText = lista_organizadores[i]['nombre_completo'];
             fila.insertCell().innerText = lista_organizadores[i]['correo_electronico'];
             fila.insertCell().innerText = lista_organizadores[i]['fecha'];
             fila.insertCell().innerText = lista_organizadores[i]['telefonos'];
             fila.insertCell().innerText = lista_organizadores[i]['genero'];
-            fila.insertCell().innerHTML = '<i class="fas fa-edit"></i>';
-            fila.insertCell().innerHTML = '<i class="fas fa-trash-alt"></i>';
+            fila.insertCell().innerHTML = '<i title="Modificar" class="fas fa-edit verde-oscuro-a--tipografia"></i>';
         }
     };
 }
--- js/registrar_organizador_eventos/listar-organizador-eventos-servicio.js
+++ js/registrar_organizador_eventos/listar-organizador-eventos-servicio.js
@@ -1,12 +1,13 @@
 let listar_organizador_evento = async () => {
     let lista_organizadores;
+    
     await axios({
         method: 'get',
         url: 'http://localhost:3000/api/listar-organizador-evento',
         responseType: 'json',
     })
     .then(function(res){
-        lista_organizadores = res.data.eventos;
+        lista_organizadores = res.data.datos;
     })
     .catch(function (error) {
         console.log(error);
