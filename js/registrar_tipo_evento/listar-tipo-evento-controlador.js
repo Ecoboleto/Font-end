697e33a15e7541190971b52da5fe839926b017e7
byron-sequeira <55303010+byron-sequeira@users.noreply.github.com>
2019-12-18 01:22:18 -0500

    Modificar tipo de evento
    Modificar tipo de evento

--- js/registrar_tipo_evento/listar-tipo-evento-controlador.js
+++ js/registrar_tipo_evento/listar-tipo-evento-controlador.js
@@ -15,6 +15,19 @@ let llenarTabla = async () => {
 
         fila.insertCell().innerHTML = lista_tipos_eventos[i]['tipo_evento'];
         fila.insertCell().innerHTML = lista_tipos_eventos[i]['estado'];
+
+        let celda_editar = fila.insertCell();
+        let boton_editar = document.createElement('button');
+
+        boton_editar.innerText = 'Editar';
+        boton_editar.dataset._id = lista_tipos_eventos[i]['_id'];
+        boton_editar.addEventListener('click', function(){
+            sessionStorage.setItem('tipo_evento_id', this.dataset._id)
+            window.location.href = 'modificar-tipo-evento.html';
+        })
+
+        celda_editar.appendChild(boton_editar);
+
         }
     };
 };
--- js/registrar_tipo_evento/tipo-evento-servicio.js
+++ js/registrar_tipo_evento/tipo-evento-servicio.js
@@ -56,3 +56,61 @@ let listar_tipo_evento = async () => {
     });
     return lista_tipo_evento;
 };
+
+let obtener_tipo_evento_id = async(pid) => {
+    try {
+
+        const response = await axios({
+            method: 'get',
+            params: { _id: pid },
+            url: 'http://localhost:3000/api/obtener-tipo-evento-id',
+            responseType: 'json'
+        });
+
+        return response.data.tipos_eventos;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+let modificar_tipo_evento = async (idtp, tipo_evento) => {
+    await axios(
+        {
+            method: 'post',
+            url: 'http://localhost:3000/api/modificar-tipo-evento',
+            responseType: 'json',
+            data: {
+                _id: idtp,
+                tipo_evento: tipo_evento,
+            }
+
+        }
+    )
+        .then(function (res) {
+            if (res.data.resultado) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Registro realizado con éxito',
+                    text: 'El tipo de evento ha sido almacenado',
+                    confirmButtonText: 'Entendido'
+                });
+
+            } else {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'No se pudo registrar el tipo de evento, el nombre ya existe',
+                    confirmButtonText: 'Entendido'
+                });
+            }
+        })
+        .catch(function (error) {
+
+            Swal.fire({
+                icon: 'warning',
+                title: 'No se puede conectar con el servidor',
+                confirmButtonText: 'Entendido'
+            });
+            console.log(error);
+
+        })
+};
