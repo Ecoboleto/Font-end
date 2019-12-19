5ee408e08faf9ce0db51da9580a329d1b88846e1
byron-sequeira <55303010+byron-sequeira@users.noreply.github.com>
2019-12-18 19:19:35 -0500

    Recintos
    Actualizacion

--- js/registrar_evento/perfil-evento-controlador.js
+++ js/registrar_evento/perfil-evento-controlador.js
@@ -40,11 +40,11 @@ let crearInfo = async () => {
     let descripcion = document.createElement('p');
     let precio = document.createElement('p');
     let limite = document.createElement('p');
-    let sel_entradas = document.createElement('label');
-    let cant_entradas = document.createElement('select');
-    let opt_entradas = document.createElement('option');
-    let carrito = document.createElement('button');
-    let comprar = document.createElement('button');
+    //let sel_entradas = document.createElement('label');
+    //let cant_entradas = document.createElement('select');
+    //let opt_entradas = document.createElement('option');
+    //let carrito = document.createElement('button');
+    //let comprar = document.createElement('button');
 
 
     imagen.src = evento['foto_evento'];
@@ -55,10 +55,10 @@ let crearInfo = async () => {
     descripcion.innerHTML = ('Descripción de evento: ' + evento['descripcion_evento']);
     precio.innerHTML = ('Precio de entrada: ₡' + evento['entrada_evento']);
     limite.innerHTML = ('Límite de entradas: ' + evento['limite_evento']);
-    sel_entradas.innerHTML = ('Seleccione la cantidad de entradas: ');
-    opt_entradas.innerHTML = ('---');
-    carrito.innerHTML = ('Agregar al carrito')
-    comprar.innerHTML = ('Comprar')
+    //sel_entradas.innerHTML = ('Seleccione la cantidad de entradas: ');
+    //opt_entradas.innerHTML = ('---');
+    //carrito.innerHTML = ('Agregar al carrito')
+    //comprar.innerHTML = ('Comprar')
 
 
     cont_evento.classList.add('evento');
@@ -71,18 +71,18 @@ let crearInfo = async () => {
     descripcion.classList.add('descripcion');
     precio.classList.add('precio');
     limite.classList.add('limite');
-    sel_entradas.classList.add('lab');
-    cant_entradas.classList.add('sel');
+    //sel_entradas.classList.add('lab');
+    //cant_entradas.classList.add('sel');
 
-    carrito.classList.add('btn');
-    carrito.classList.add('btn--accion');
-    carrito.classList.add('btn--positivo');
-    carrito.classList.add('blanco--tipografia');
+    //carrito.classList.add('btn');
+    //carrito.classList.add('btn--accion');
+    //carrito.classList.add('btn--positivo');
+    //carrito.classList.add('blanco--tipografia');
 
-    comprar.classList.add('btn');
-    comprar.classList.add('btn--accion');
-    comprar.classList.add('btn--positivo');
-    comprar.classList.add('blanco--tipografia');
+    //comprar.classList.add('btn');
+    //comprar.classList.add('btn--accion');
+    //comprar.classList.add('btn--positivo');
+    //comprar.classList.add('blanco--tipografia');
 
 
 
@@ -96,17 +96,17 @@ let crearInfo = async () => {
     cont_evento.appendChild(descripcion);
     cont_evento.appendChild(precio);
     cont_evento.appendChild(limite);
-    cont_evento.appendChild(sel_entradas);
-    cont_evento.appendChild(cant_entradas);
-    cant_entradas.appendChild(opt_entradas);
-    let cant_limite = Number(evento['limite_evento']);
-    for (let i = 1; i <= cant_limite; i++) {
-        let opt_entradas = document.createElement('option');
-        opt_entradas.innerHTML = i;
-        cant_entradas.appendChild(opt_entradas);
-    }
-    cont_evento.appendChild(carrito);
-    cont_evento.appendChild(comprar);
+    //cont_evento.appendChild(sel_entradas);
+    //cont_evento.appendChild(cant_entradas);
+    //cant_entradas.appendChild(opt_entradas);
+    //let cant_limite = Number(evento['limite_evento']);
+    //for (let i = 1; i <= cant_limite; i++) {
+    //    let opt_entradas = document.createElement('option');
+    //    opt_entradas.innerHTML = i;
+    //    cant_entradas.appendChild(opt_entradas);
+    //}
+    //cont_evento.appendChild(carrito);
+    //cont_evento.appendChild(comprar);
 }
 
 crearInfo();
